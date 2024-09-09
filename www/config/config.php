e7b656e6e0b30c181c59008c52bd5fbf1b4b7394
Sebastian Plaza <splaza76@gmail.com>
2024-09-09 10:21:25 +0200

    inkl. PHPMailer

--- www/config/config.php
+++ www/config/config.php
@@ -1,4 +1,8 @@
 <?php
+header('Access-Control-Allow-Origin: *');
+header('Access-Control-Allow-Methods: GET, POST');
+header("Access-Control-Allow-Headers: X-Requested-With");
+
 // Copyright (C) 2015 Sebastian Plaza
 // This program is free software: you can redistribute it and/or modify it under the terms of the GNU General Public License
 // as published by the Free Software Foundation, either version 3 of the License, or (at your option) any later version.
