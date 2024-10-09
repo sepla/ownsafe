95ee4f86bea129cecbb537bfdff318298f11adfd
Sebastian Plaza <splaza76@gmail.com>
2024-10-09 09:59:31 +0200

    install-app

--- docker/install-app.sh
+++ docker/install-app.sh
@@ -1,12 +1,8 @@
 #!/bin/bash
 echo "Starting app installation"
 cd /
-if [ ! -f "/ownsafe/www/index.html" ]; then
-    git clone https://github.com/sepla/ownsafe.git 
-fi
 cp -r /ownsafe/www/* /var/www/html
-cp /ownsafe/docker/php/php-logging.conf /usr/local/etc/php-fpm.d/zz-log.conf
-mkdir -p /etc/nginx/conf.d
-cp /ownsafe/docker/nginx-conf/* /etc/nginx/conf.d -r
+cp /ownsafe/docker/nginx-conf/* /docker/nginx-conf/ -r
+cp /ownsafe/docker/php/php-logging.conf /docker/php/
 echo "App installation finisched"
 
