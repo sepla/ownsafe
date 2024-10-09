bf40d55690b3710c34d788c89790ce19a4cbfa30
Sebastian Plaza <splaza76@gmail.com>
2024-10-09 10:19:45 +0200

    install-app

--- docker/install-app.sh
+++ docker/install-app.sh
@@ -3,6 +3,6 @@ echo "Starting app installation"
 cd /
 cp -r /ownsafe/www/* /var/www/html
 cp /ownsafe/docker/nginx-conf/* /docker/nginx-conf/ -r
-cp /ownsafe/docker/php/php-logging.conf /docker/php/
+#cp /ownsafe/docker/php/php-logging.conf /docker/php/php-logging.conf
 echo "App installation finisched"
 
