d301039896faa9f87553a1c7e1dd9da1c3475e92
Sebastian Plaza <splaza76@gmail.com>
2024-10-09 16:01:07 +0200

    Ok?

--- www/js/index.js
+++ www/js/index.js
@@ -65,6 +65,19 @@ if(typeof(Storage) === "undefined") {
 
 sessionStorage.setItem("ver","Ver. "+ver);
 console.log("OwnSafe - "+sessionStorage.getItem("ver"));
+       
+var serverUrl = getCookie("OwnSafeServerUrl");
+if (serverUrl && serverUrl.trim().length > 0) {
+        localStorage.setItem('OwnSafeServerUrl',serverUrl);
+        sessionStorage.setItem('OwnSafeServerUrl',serverUrl);
+        console.log("ServerURL: "+sessionStorage.getItem('OwnSafeServerUrl'));
+} else if (localStorage.getItem('OwnSafeServerUrl') && localStorage.getItem('OwnSafeServerUrl').trim().length !== 0) {
+
+        console.log("LS ServerURL: "+localStorage.getItem('OwnSafeServerUrl'));
+        sessionStorage.setItem('OwnSafeServerUrl',localStorage.getItem('OwnSafeServerUrl'));
+}
+
+// DB init
 if (window.sessionStorage.getItem('OwnSafeServerUrl') && window.sessionStorage.getItem('OwnSafeServerUrl').trim().length > 0) {
     $.ajax({
         url: sessionStorage.getItem('OwnSafeServerUrl')+'/src/init.php',
@@ -73,20 +86,11 @@ if (window.sessionStorage.getItem('OwnSafeServerUrl') && window.sessionStorage.g
             console.log("DB Init: "+result);
         },
         error:function(xhr,status,error){
+
             console.log("DB Init: "+xhr+" STATUS: "+status+" ERROR:"+error);
         }
     });
 } else console.log("DB Init: follow");
-        
-var serverUrl = getCookie("OwnSafeServerUrl");
-if (serverUrl && serverUrl.trim().length > 0) {
-        localStorage.setItem('OwnSafeServerUrl',serverUrl);
-        sessionStorage.setItem('OwnSafeServerUrl',serverUrl);
-        console.log("ServerURL: "+sessionStorage.getItem('OwnSafeServerUrl'));
-} else if (localStorage.getItem('OwnSafeServerUrl') && localStorage.getItem('OwnSafeServerUrl').trim().length !== 0) {
-        console.log("LS ServerURL: "+localStorage.getItem('OwnSafeServerUrl'));
-        sessionStorage.setItem('OwnSafeServerUrl',localStorage.getItem('OwnSafeServerUrl'));
-}
 
 var languageChoiceString = '<option value="EN">English</option><option value="DE">Deutsch</option><option value="PL">Polski</option>';       
 
@@ -106,6 +110,7 @@ if (language && language.trim().length > 0) {
 if (window.localStorage.getItem('uiDarkMode') && window.localStorage.getItem('uiDarkMode').trim().length > 0) {
     console.log("LS uiDarkMode: "+window.localStorage.getItem('uiDarkMode'));        
     window.sessionStorage.setItem('uiDarkMode',window.localStorage.getItem('uiDarkMode'));    
+
 } else {
     window.localStorage.setItem('uiDarkMode','true');
     window.sessionStorage.setItem('uiDarkMode','true');
@@ -115,11 +120,13 @@ if (window.localStorage.getItem('uiDarkMode') && window.localStorage.getItem('ui
         
 $(document).on('pageinit', '#login', function(){
     if (sessionStorage.getItem('OwnSafeServerUrl') && sessionStorage.getItem('OwnSafeServerUrl')!=="null" && sessionStorage.getItem('OwnSafeServerUrl').trim().length > 0) {
+
          $('#login').load(sessionStorage.getItem('OwnSafeServerUrl')+'/src/page_login.php', function(data, textStatus, jqXHR){
             console.log("Init: "+textStatus);
             if (textStatus === "success" || textStatus === "notmodified") {
                 $('#login').trigger('create');
                 $.mobile.changePage("#login");
+
                 checkLoginHints();
                 checkMenuButtonHilight();
             } else {
@@ -136,10 +143,12 @@ $(document).on('pageinit', '#index', function(){
      checkInstallStatus();    
      $(document).on('click', '#submit_button', function() {
 
+
         if($('#url').length && $('#url').val().length > 0){
             var url = $('#url').val().trim();
             if (url.includes=="#") url = url.substring(0, url.indexOf("#"));
             if (url.slice(-1)=="/") url.slice(0, -1);
+
             
 			var initUrl = url+'/src/init.php';
             console.log("Trýing to connect to "+initUrl);
@@ -152,18 +161,21 @@ $(document).on('pageinit', '#index', function(){
                     data: formdata,
                     dataType: 'json',
                     beforeSend: function() {$.mobile.loading("show");},
+
                     complete: function() {$.mobile.loading("hide");},
                     success: function (result) {
                         if (result.status == "OK") {
                             window.localStorage.setItem('OwnSafeServerUrl',url);
                             window.localStorage.setItem('OwnSafeLanguage',language);
                             window.sessionStorage.setItem('OwnSafeServerUrl',url);
+
                             window.sessionStorage.setItem('OwnSafeLanguage',language);
                             $("#error").html("");
                             console.log("Submit OK");
                             $('#login').load(sessionStorage.getItem('OwnSafeServerUrl')+"/src/page_login.php",function(){
                                 $('#login').trigger('create');
                                 $.mobile.changePage("#login");
+
                                 checkLoginHints();
                                 checkMenuButtonHilight();
                                 $("#serverUrlInput").hide();
@@ -177,6 +189,7 @@ $(document).on('pageinit', '#index', function(){
                             $.mobile.loading("hide");
                             $("#serverUrlInput").fadeIn();
                             $("#error").html(html);
+
                             
                         }
                     },
@@ -188,6 +201,7 @@ $(document).on('pageinit', '#index', function(){
                         $.mobile.loading("hide");
                         $("#serverUrlInput").fadeIn();
                         $("#error").html(html);
+
                     }			
 				});
 			} else console.log("No language");
@@ -200,8 +214,11 @@ $(document).on('pageinit', '#index', function(){
 });
 
 $(document).on('click', '#index_url_href',function(event) {
+
     if (isApp) {
+
         window.open($('#index_url_href').attr('href'), '_system','location=yes');
+
         return false;
     }
 });
@@ -213,6 +230,7 @@ $(document).on('click', '#index_install', function(event) {
 $(document).on('click', '#index_download_osfb', function(event) {
 			var link = "http://www.diahal.de/ownsafeApp/INSTALL/ownsafe_for_browser.tar.gz";
 			if (isApp) window.open(link, '_system');
+
             else  window.open(link, '_blank');
 });
 
@@ -220,15 +238,20 @@ $(document).on('click', '#index_download_os', function(event) {
 			var link = "/INSTALL/ownsafe.tar.gz";
 			if (isApp) window.open(link, '_system');
             else  window.open(link, '_blank');
+
 });
 
 // <!-- ----- hilight buttos ----- -->
 $( document ).on( "pageshow" , '#install, #index', function ( event, data ) {
+
     
+
 	var button = $.mobile.activePage.attr("id");
 	$(".index_settings_button").removeClass( "hilightbackground" );
+
 	$(".index_install_button").removeClass( "hilightbackground" );
 	switch(button) {
+
 	case "index":
         $('#menu_indexfooter_text').html("OwnSafe - "+sessionStorage.getItem("ver"));
 		$(".index_settings_button").addClass( "hilightbackground" );
